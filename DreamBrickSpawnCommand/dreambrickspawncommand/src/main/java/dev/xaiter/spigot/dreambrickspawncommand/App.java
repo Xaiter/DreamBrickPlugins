4888e643a967ec3e761456679dc21a0ac197d657
Xaiter <xaiter@gmail.com>
2019-06-14 21:26:47 -0700

    oh boy spawn commands

---
+++ DreamBrickSpawnCommand/dreambrickspawncommand/src/main/java/dev/xaiter/spigot/dreambrickspawncommand/App.java
@@ -0,0 +1,142 @@
+package dev.xaiter.spigot.dreambrickspawncommand;
+
+import java.util.Collection;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedEnterEvent;
+import org.bukkit.event.player.PlayerBedEnterEvent.BedEnterResult;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Score;
+import org.bukkit.scoreboard.Scoreboard;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.TextComponent;
+
+public class App extends JavaPlugin implements Listener, CommandExecutor {
+
+    private final int TELEPORT_COST = 5;
+    private final String MSG_COLLECTIVE_DREAM = "You experience a collective dream...";
+    private final String MSG_TELEPORT_FAILED_NOT_ASLEEP = "You must be asleep to teleport to spawn! (Enter a bed)";
+    private final String MSG_TELEPORT_FAILED_CANNOT_AFFORD = "You need at least " + TELEPORT_COST + " vote flint to return to spawn! (Sorry!)";
+    private final String BALANCE_SCOREBOARD_NAME = "balance";
+
+    @Override
+    public void onEnable() {
+        PluginManager manager = getServer().getPluginManager();
+        manager.registerEvents(this, this);
+        this.getCommand("spawn").setExecutor(this);
+    }
+
+    @Override
+    public void onDisable() {
+        HandlerList.unregisterAll((Listener) this);
+    }
+
+    @EventHandler
+    public void onPlayerBedEnterEvent(PlayerBedEnterEvent e) {
+        // If they didn't successfully enter a bed, we can't possibly be in the correct state.
+        if(e.getBedEnterResult() != BedEnterResult.OK)
+            return;
+
+        if(AreAllPlayersAsleep()) {
+            Server s = Bukkit.getServer();
+            Collection<? extends Player> onlinePlayers = s.getOnlinePlayers();
+
+            for(Player p : onlinePlayers) {
+                TeleportToSpawn(s, p);
+                MessagePlayer(p, MSG_COLLECTIVE_DREAM, ChatColor.LIGHT_PURPLE);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player p = GetPlayer(sender.getName());
+
+        // Wait, what?  Who sent this command?
+        if(p == null) 
+            return false;
+
+        // Hey!  You need to be asleep to dream!
+        if(!p.isSleeping()) {
+            MessagePlayer(p, MSG_TELEPORT_FAILED_NOT_ASLEEP, ChatColor.RED);
+            return false;
+        }
+
+        // Okay, check if the player can afford it, and charge them if they can!
+        Server s = Bukkit.getServer();
+        if(!TryPayForTeleport(s, p))
+            return false;
+        
+        // Finally, teleport the player and exit
+        TeleportToSpawn(s, p);
+        return true;
+    }
+
+    private Player GetPlayer(String name) {
+        Collection<? extends Player> players = GetOnlinePlayers();
+        for(Player p : players) {
+            if(p.getName().equalsIgnoreCase(name))
+                return p;
+        }
+        return null;
+    }
+
+    private void TeleportToSpawn(Server s, Player p) {
+        s.dispatchCommand(s.getConsoleSender(), "warp overworld_spawn" +  p.getName());
+    }
+
+    private boolean TryPayForTeleport(Server s, Player p) {
+        Objective o = s.getScoreboardManager().getMainScoreboard().getObjective(BALANCE_SCOREBOARD_NAME);
+        Score score = o.getScore(p.getName());
+        
+        // Can't afford it, we're done
+        int balance = score.getScore();
+        if(balance < TELEPORT_COST) {
+            MessagePlayer(p, MSG_TELEPORT_FAILED_CANNOT_AFFORD, ChatColor.RED);
+            return false;
+        }
+        
+        // Charge the player!
+        score.setScore(balance - TELEPORT_COST);
+        return true;
+    }
+
+    private void MessagePlayer(Player p, String text, ChatColor color) {
+        TextComponent message = new TextComponent(text);
+        message.setBold(true);
+        message.setColor(color);
+        p.spigot().sendMessage(message);
+    }
+
+    private boolean AreAllPlayersAsleep() {
+        // Get all the players...
+        Server s = Bukkit.getServer();
+        Collection<? extends Player> onlinePlayers = s.getOnlinePlayers();
+
+        // Is everyone asleep?
+        for(Player p : onlinePlayers) {
+            // Ops don't count.
+            if(!p.isSleeping() && !p.isOp())
+                return false;
+        }
+
+        // Yep.
+        return true;
+    }
+
+    private Collection<? extends Player> GetOnlinePlayers() {
+        Server s = Bukkit.getServer();
+        return s.getOnlinePlayers();
+    }
+}
