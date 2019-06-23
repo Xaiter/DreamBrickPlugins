caf7207ec4fc92b8b9e02833a6588427a357a043
HX7Q <epicme68@gmail.com>
2019-06-23 18:22:31 -0300

    everything touching a file closes properly

--- DreamBrickSpawnCommand/dreambrickspawncommand/src/main/java/dev/xaiter/spigot/dreambrickspawncommand/NameFilter.java
+++ DreamBrickSpawnCommand/dreambrickspawncommand/src/main/java/dev/xaiter/spigot/dreambrickspawncommand/NameFilter.java
@@ -38,8 +38,11 @@ public class NameFilter implements Listener {
         //file path
         File file = new File(this.plugin.getDataFolder().toString() + File.separator + "players" + File.separator + playerUUID + ".txt");
         
+        //declare as null
+        BufferedReader reader = null;
+
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(file));
+            reader = new BufferedReader(new FileReader(file));
 
             //read file (name previously associated with player's uuid)
             String oldName = reader.readLine();
@@ -53,34 +56,72 @@ public class NameFilter implements Listener {
                 for (Objective o : scoreboard.getObjectives()){
                     s.dispatchCommand(consoleSender, "execute store result score " + p.getName() + " " + o.getName() + " run scoreboard players get " + oldName + " " + o.getName());
                 }
-                s.dispatchCommand(consoleSender, "scoreboard players reset " + oldName);
+                //reset scores?
+                //s.dispatchCommand(consoleSender, "scoreboard players reset " + oldName);
 
                 //overrides old name with new name
-                createNewFile(file, p);
+                writeFile(file, p);
             }
-            reader.close();
         } catch (FileNotFoundException e) {
             //welcome message
             s.dispatchCommand(consoleSender, "tellraw @a [{\"text\":\"Welcome \",\"color\":\"gray\",\"bold\":\"false\"},{\"text\":\"" + p.getName() + "\",\"color\":\"gold\",\"bold\":\"true\"},{\"text\":\" to Dream's End!\",\"color\":\"gray\",\"bold\":\"false\"}]");
             
-            //sets balance to zero
+            //sets balance to zero (so /bal will always show a number)
             s.dispatchCommand(consoleSender, "scoreboard players set " + p.getName() + " balance 0");
-            
+
+            //anything else that should happen when a player first joins the server
+
             //creates a new file
-            createNewFile(file, p);
+            writeFile(file, p);
         } catch (IOException e){
             e.printStackTrace();
+        } finally {
+            //always try closing
+            tryCloseReader(reader);
         }
     }
 
-    public static void createNewFile(File file, Player p){
+    public static void writeFile(File file, Player p){
+
+        //declare as null
+        BufferedWriter writer = null;
+
         try {
             //writes player name to file
-            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            writer = new BufferedWriter(new FileWriter(file));
             writer.write(p.getName());
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            //always try closing
+            tryCloseWriter(writer);
+        }
+    }
+
+    public static void tryCloseReader(BufferedReader reader){
+        //does reader exist
+        if (reader != null){
+            try {
+                //try closing no matter what
+                reader.close();
+            } catch (IOException e) {
+                //dont care
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void tryCloseWriter(BufferedWriter writer){
+        //does writer exist
+        if (writer != null){
+            try {
+                //try closing no matter what
+                writer.close();
+            } catch (IOException e) {
+                //dont care
+                e.printStackTrace();
+            }
         }
     }
 
