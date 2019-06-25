f38e911d5c45b0158fc058b7bd4487cd40ca5516
HX7Q <epicme68@gmail.com>
2019-06-24 22:34:38 -0300

    players dont keep their teams when changing names... fixed

--- DreamBrickSpawnCommand/dreambrickspawncommand/src/main/java/dev/xaiter/spigot/dreambrickspawncommand/NameFilter.java
+++ DreamBrickSpawnCommand/dreambrickspawncommand/src/main/java/dev/xaiter/spigot/dreambrickspawncommand/NameFilter.java
@@ -9,12 +9,14 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.Server;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Score;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 
@@ -32,8 +34,8 @@ public class NameFilter implements Listener {
         ConsoleCommandSender consoleSender = Bukkit.getConsoleSender();
 
         //get player
-        Player p = event.getPlayer();
-        String playerUUID = p.getUniqueId().toString();
+        Player player = event.getPlayer();
+        String playerUUID = player.getUniqueId().toString();
 
         //file path
         File file = new File(this.plugin.getDataFolder().toString() + File.separator + "players" + File.separator + playerUUID + ".txt");
@@ -48,37 +50,72 @@ public class NameFilter implements Listener {
             String oldName = reader.readLine();
 
             //if old name is not equal to new name...
-            if (!oldName.equals(p.getName())){
+            if (!oldName.equals(player.getName())){
 
                 Scoreboard scoreboard = s.getScoreboardManager().getMainScoreboard();
                 
                 //store old scoreboard into new
                 for (Objective o : scoreboard.getObjectives()){
-                    s.dispatchCommand(consoleSender, "execute store result score " + p.getName() + " " + o.getName() + " run scoreboard players get " + oldName + " " + o.getName());
+                    s.dispatchCommand(consoleSender, "execute store result score " + player.getName() + " " + o.getName() + " run scoreboard players get " + oldName + " " + o.getName());
                 }
                 //reset scores?
                 //s.dispatchCommand(consoleSender, "scoreboard players reset " + oldName);
 
+                //gets team for player's old name
+                Team team = getTeamForPlayerName(scoreboard, oldName, playerUUID);
+                
+                //old player leaves team
+                s.dispatchCommand(consoleSender, "team leave " + oldName);
+
+                //if the old name was on a team, make the new name join the team
+                if (team != null){
+                    s.dispatchCommand(consoleSender, "team join " + team.getName() + " " + player.getName());
+                }
+                //if no team was found, kick the player out
+                else {
+                    s.dispatchCommand(consoleSender, "team leave " + player.getName());
+                }
+
                 //overrides old name with new name
-                writeFile(file, p);
+                writeFile(file, player);
             }
         } catch (FileNotFoundException e) {
             //welcome message
-            s.dispatchCommand(consoleSender, "tellraw @a [{\"text\":\"Welcome \",\"color\":\"gray\",\"bold\":\"false\"},{\"text\":\"" + p.getName() + "\",\"color\":\"gold\",\"bold\":\"true\"},{\"text\":\" to Dream's End!\",\"color\":\"gray\",\"bold\":\"false\"}]");
+            s.dispatchCommand(consoleSender, "tellraw @a [{\"text\":\"Welcome \",\"color\":\"gray\",\"bold\":\"false\"},{\"text\":\"" + player.getName() + "\",\"color\":\"gold\",\"bold\":\"true\"},{\"text\":\" to Dream's End!\",\"color\":\"gray\",\"bold\":\"false\"}]");
             
             //sets balance to zero (so /bal will always show a number)
-            s.dispatchCommand(consoleSender, "scoreboard players set " + p.getName() + " balance 0");
+            s.dispatchCommand(consoleSender, "scoreboard players set " + player.getName() + " balance 0");
 
             //anything else that should happen when a player first joins the server
 
             //creates a new file
-            writeFile(file, p);
+            writeFile(file, player);
         } catch (IOException e){
             e.printStackTrace();
         } finally {
             //always try closing
             tryCloseReader(reader);
         }
+    }
+
+    public static Team getTeamForPlayerName(Scoreboard scoreboard, String name, String uuid){
+
+        Team team = null;
+
+        //loop through every team
+        for (Team t : scoreboard.getTeams()){
+            //loop through every player
+            for (OfflinePlayer p : t.getPlayers()){
+                //return team if names match and uuids match (in case two players have had the same past names)
+                if (name.equals(p.getName()) && uuid.equals(p.getUniqueId().toString())){
+                    return t;
+                }
+            }
+        }
+
+        //team is null if no team is found
+        return team;
+
     }
 
     public static void writeFile(File file, Player p){
